1baff206cd15f144475d46e2133a0b64eccad531
Melodie <meisselmelodie@gmail.com>
2019-06-06 14:49:13 +0200

    Projet

---
+++ src/main/java/com/training/spring/bigcorp/BigcorpApplication.java
@@ -0,0 +1,13 @@
+package com.training.spring.bigcorp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigcorpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigcorpApplication.class, args);
+	}
+
+}
